62626fe7a6f3a0011ade375295c84ba1118805d8
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2016-09-13 15:18:54 +0900

    Unity 5.4へ移行、RingCommandサンプルを追加

---
+++ Assets/RingCommand/Script/PlayAnimationClip.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimationClip : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/RingCommand/Script/RingCommand.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class RingCommand : UIBehaviour, ILayoutController 
+{
+	public float radius = 100;
+	public float offsetAngle;
+
+	protected override void OnValidate ()
+	{
+		base.OnValidate ();
+		Arrange ();
+	}
+
+	// 要素数が変わると自動的に呼ばれるコールバック
+#region ILayoutController implementation
+	public void SetLayoutHorizontal (){}
+	public void SetLayoutVertical ()
+	{
+		Debug.Log("hoge");
+		Arrange ();
+	}
+#endregion
+
+	public void Arrange()
+	{
+		float splitAngle = 360 / transform.childCount;
+
+		for (int elementId = 0; elementId < transform.childCount; elementId++) {
+			var child = transform.GetChild (elementId) as RectTransform;
+			float currentAngle = splitAngle * elementId + offsetAngle;
+			child.anchoredPosition = new Vector2 (
+				Mathf.Cos (currentAngle * Mathf.Deg2Rad), 
+				Mathf.Sin (currentAngle * Mathf.Deg2Rad)) * radius;
+		}
+	}
+}
